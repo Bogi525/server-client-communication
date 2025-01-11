7cd32b282e8244b005cc07629bb8ee2305f67fd8
Bogi525 <bogithe525@gmail.com>
2025-01-11 12:37:43 +0100

    Followed javidx9 tutorial for ASIO(first 15 mins)

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include "../inc/logEntry.hpp"
+
+#define ASIO_STANDALONE
 #include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
 
 
 int main() {
-    LogEntry* entry1 = new LogEntry("bla", EntryType::EntryTypeMSG, "blabla");
-    
-    asio::io_context io_context;
-    std::cout << "ASIO setup successful!\n";
 
-    int a;
-    std::cin >> a;
+    //Initialize an ASIO error 
+    asio::error_code ec;
+    
+    asio::io_context context;
+
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("192.168.0.1", ec), 80);
+
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+
+    if (!ec) {
+        std::cout << "Connected!\n";
+    } else {
+        std::cout << "Failed to connect.\n";
+        std::cout << ec.message() << "\n";
+    }
+    
+    system("pause");
+
     return 0;
 }
