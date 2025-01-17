067d93c01d817b6b0b1fe03cd6fdbb2d5960376a
Bogi525 <bogithe525@gmail.com>
2025-01-17 17:38:05 +0100

    Code clean-up

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -42,15 +42,21 @@ int main() {
         bool finished = false;
 
         while(!finished) {
+
             std::cout << "Input:";
             std::getline(std::cin, output_message);
+
             if (output_message.length() > 1024) {
+
                 cout << "Message must have less than 1024 characters!\n";
                 continue;
+
             } else if (output_message == "!finish") {
+                
                 cout << "Finishing communication!\n";
                 finished = true;
                 break;
+                
             } else {
                 asio::write(socket, asio::buffer(output_message));
                 std::cout << "Sent!\n";
@@ -59,59 +65,9 @@ int main() {
 
         socket.close();
 
-        // // Initalizing ASIO error and context
-        // asio::error_code ec;
-        // asio::io_context context;
-
-        // // Set the socket for IPv4 communication
-        // asio::ip::udp::socket socket(context);
-        // socket.open(asio::ip::udp::v4());
-
-        // // Allow LAN broadcast communication
-        // asio::socket_base::broadcast option(true);
-        // socket.set_option(option);
-
-        // // Set up a broadcast endpoint
-        // asio::ip::udp::endpoint broadcast_endpoint(asio::ip::address_v4::broadcast(), 12345);
-
-        // // Broadcasting the message across LAN
-        // std::string messageToSend = "This is a broadcast message. Yippie!";
-        // socket.send_to(asio::buffer(messageToSend), broadcast_endpoint);
     } catch (std::exception& e) {
         std::cout << "Error: " << e.what() << '\n';
     }
-    
-
-    // socket.connect(endpoint, ec);
-
-    // if (!ec) {
-    //     std::cout << "Connected!\n";
-    // } else {
-    //     std::cout << "Failed to connect.\n";
-    //     std::cout << ec.message() << "\n";
-    // }
-
-    // if (socket.is_open()) {
-    //     std::string sRequest =
-    //         "GET /index.html HTTP/1.1\r\n"
-    //         "Host: example.com\r\n"
-    //         "Connection: close\r\n\r\n";
-
-    //     socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
-
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
-    //     size_t bytes = socket.available();
-    //     std::cout << "Bytes Available: " << bytes << std::endl;
-
-    //     if (bytes > 0) {
-    //         std::vector<char> vBuffer(bytes);
-    //         socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-
-    //         for (auto c: vBuffer)
-    //             std::cout << c;
-    //     }
-    //}
 
     system("pause");
 
--- src/main_server.cpp
+++ src/main_server.cpp
@@ -46,6 +46,7 @@ int main() {
             bool finished = false;
 
             while(!finished) {
+
                 data_length = socket.read_some(asio::buffer(incoming_data));
                 LogEntry newLogEntry("User", EntryType::EntryTypeMSG, string(incoming_data, data_length));
                 std::cout << newLogEntry.getUser() << "(" << newLogEntry.getTime() << "): " << newLogEntry.getMessage() << '\n';
@@ -54,27 +55,9 @@ int main() {
             }
 
             socket.close();
+            
         }
 
-
-
-    //     // Initalizing ASIO error and context
-    //     asio::error_code ec;
-    //     asio::io_context context;
-
-    //     asio::ip::udp::socket socket(context, asio::ip::udp::endpoint(asio::ip::udp::v4(), 12345));
-
-    //     char data[1024];
-    //     asio::ip::udp::endpoint sender_endpoint;
-
-    //     std::cout << "Server is starting to listen for incoming traffic on port 12345";
-
-    //     while(true) {
-    //         std::size_t bytes = socket.receive_from(asio::buffer(data, sizeof(data)),sender_endpoint);
-    //         std::string message(data, bytes);
-    //         std::cout << "Received message from \"" << sender_endpoint.address() << "\", size = " << bytes << ":\n";
-    //         std::cout << message << '\n';
-    //     }
     } catch (std::exception& e) {
         std::cout << "Error: " << e.what() << '\n';
     }
@@ -82,48 +65,4 @@ int main() {
     system("pause");
 
     return 0;
-
-    // // Initialize an ASIO error 
-    // asio::error_code ec;
-    
-    // asio::io_context context;
-
-    // asio::ip::tcp::endpoint endpoint(asio::ip::make_address(IP_DEST, ec), 80);
-
-    // asio::ip::tcp::socket socket(context);
-
-    // socket.connect(endpoint, ec);
-
-
-
-    // if (!ec) {
-    //     std::cout << "Connected!\n";
-    // } else {
-    //     std::cout << "Failed to connect.\n";
-    //     std::cout << ec.message() << "\n";
-    // }
-
-    // if (socket.is_open()) {
-    //     std::string sRequest =
-    //         "GET /index.html HTTP/1.1\r\n"
-    //         "Host: example.com\r\n"
-    //         "Connection: close\r\n\r\n";
-
-    //     socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
-
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
-    //     size_t bytes = socket.available();
-    //     std::cout << "Bytes Available: " << bytes << std::endl;
-
-    //     if (bytes > 0) {
-    //         std::vector<char> vBuffer(bytes);
-    //         socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-
-    //         for (auto c: vBuffer)
-    //             std::cout << c;
-    //     }
-    // }
-    
-    
 }
