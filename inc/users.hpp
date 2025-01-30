c5f4aa423c79cdecb7deb44f47917eeb7509ce81
Bogi525 <bogithe525@gmail.com>
2025-01-30 16:44:27 +0100

    Added ability to read and write in users.csv

--- inc/users.hpp
+++ inc/users.hpp
@@ -1,7 +1,8 @@
-#ifndef _accounts_hpp_
-#define _accounts_hpp_
+#ifndef _users_hpp_
+#define _users_hpp_
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include "user.hpp"
 
@@ -10,13 +11,16 @@ class Users {
 public:
     Users();
 
-    Users(const Users&) = delete;
-    Users& operator=(const Users&) = delete;
+    //Users(const Users&) = delete;
+    //Users& operator=(const Users&) = delete;
     
     User getUser(std::string username);
+    bool createUser(std::string username, std::string password);
     
 private:
+    bool addUserToFile(std::string username, std::string password);
     std::vector<User> users;
+    User nullUser;
 };
 
 #endif
--- src/users.cpp
+++ src/users.cpp
@@ -1,13 +1,54 @@
 #include "../inc/users.hpp"
 
-Users::Users() {
-    User nullUser("/NULLUSERNAME", "/NULLPASSWORD");
-    users.push_back(nullUser);
+Users::Users() : nullUser("/NULLUSERNAME", "/NULLPASSWORD") {
+    fstream file("users.csv", std::ios_base::in);
+
+    std::string line;
+
+    for (int i = 0; i < 2; i++) {
+        file >> line;
+        cout << line;
+    }
+
+    file.close();
 }
 
 User Users::getUser(std::string username) {
+    if (username == "/NULLUSERNAME") return nullUser;
+
     for (int i = 0; i < users.size(); i++) {
         if (users[i].getUsername() == username) return users[i];
     }
-    return users[0];
+
+    return nullUser;
+}
+
+bool Users::createUser(std::string username, std::string password) {
+    for (int i = 0; i < users.size(); i++) {
+        if (users[i].getUsername() == username) return false;
+    }
+
+    if (addUserToFile(username,password)) {
+
+        User newUser(username, password);
+        users.push_back(newUser);
+
+        return true;
+
+    } else {
+
+        std::cout << "Error: user couldn't be registered\n";
+        return false; 
+
+    }
+}
+
+bool Users::addUserToFile(std::string username, std::string password) {
+    fstream file("users.csv", std::ios_base::app);
+
+    file << '\n' << username << "," << password;
+
+    file.close();
+
+    return true;
 }
